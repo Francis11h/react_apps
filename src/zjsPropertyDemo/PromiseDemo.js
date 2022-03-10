aa87e13fe18c6186233db1094a90e39504ac6f6b
Francis <710892027dongyi@gmail.com>
2022-03-10 15:22:50 +0800

    difference between await Promise.all() and multiple await?

---
+++ src/zjsPropertyDemo/PromiseDemo.js
@@ -0,0 +1,2 @@
+//Any difference between await Promise.all() and multiple await?
+//https://stackoverflow.com/questions/45285129/any-difference-between-await-promise-all-and-multiple-await/54291660#54291660
