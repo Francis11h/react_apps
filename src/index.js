90963a9a5dc739de16e6f54d36ffb07c8a552267
Francis <710892027dongyi@gmail.com>
2022-03-17 15:19:52 +0800

    add DatePickerDemo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import Example from './useHooksDemo/Example'
 import UseEffectDemo from './useHooksDemo/UseEffectDemo'
 import UseContextDemo from './useHooksDemo/UseContextDemo'
+import DatePickerDemo from './antd-demo/DatePickerDemo'
 
 ReactDOM.render(
     // <Example />,
     // <UseEffectDemo />,
-    <UseContextDemo />,
+    // <UseContextDemo />,
+    <DatePickerDemo />,
     document.getElementById('root')
   );
