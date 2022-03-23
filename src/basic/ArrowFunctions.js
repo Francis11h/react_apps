63d3ff40811321ed8ba4d6a85aa9542055bed235
Francis <710892027dongyi@gmail.com>
2022-03-23 16:35:44 +0800

    arrow methods can easily use DOM-level methods

--- src/basic/ArrowFunctions.js
+++ src/basic/ArrowFunctions.js
@@ -23,4 +23,35 @@ console.log(arrowfunc2(1));
 console.log(arrowfunc3(1));
 
 
-//use `node ArrowFunctions.js` to run
+//Perhaps the greatest benefit of using Arrow functions is with DOM-level methods (setTimeout, setInterval, addEventListener) that usually required some kind of closure, call, apply or bind to ensure the function executed in the proper scope.
+
+var obj = {
+  count : 10,
+  doSomethingLater : function (){
+      setTimeout(function(){ // the function executes on the window scope
+          this.count++;
+          console.log(this.count);
+      }, 300);
+  }
+}
+
+obj.doSomethingLater(); // console prints "NaN", because the property "count" is not in the window scope.
+
+
+
+var obj = {
+  count : 10,
+  doSomethingLater : function(){
+      // The traditional function binds "this" to the "obj" context.
+      setTimeout( () => {
+          // Since the arrow function doesn't have its own binding and
+          // setTimeout (as a function call) doesn't create a binding
+          // itself, the "obj" context of the traditional function will
+          // be used within.
+          this.count++;
+          console.log(this.count);
+      }, 3000);
+  }
+}
+
+obj.doSomethingLater();
