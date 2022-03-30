6f07a890e56b1a773212dd8bfc4b5873d2bc0dbe
Francis <710892027dongyi@gmail.com>
2022-03-30 14:53:46 +0800

    union type & generic type

---
+++ src/aaaTypescriptFundamental/typesFundamental.ts
@@ -0,0 +1,20 @@
+
+// union type
+// function printId(id: number | string) {
+//   console.log("Your ID is: " + id);
+// }
+// printId(10)
+// printId("francis")
+
+
+// 范型
+interface Box<T> {
+  contents: T;
+}
+let boxA: Box<string> = { contents: "hello" };
+console.log(boxA.contents)
+
+
+
+
+
