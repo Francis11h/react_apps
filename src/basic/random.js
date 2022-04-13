dfdc1bfc0ce6413a090e34c00c3c49b1b59354fe
Francis <710892027dongyi@gmail.com>
2022-04-13 20:46:46 +0800

    doge to the mooonnnnn

---
+++ src/basic/random.js
@@ -0,0 +1,7 @@
+
+const a = new Date().getTime()
+
+const b = Math.random() 
+
+console.log(a)
+console.log(b)
---
+++ src/doge/doge.js
@@ -0,0 +1,37 @@
+// o.push([e.id,
+  
+  
+  
+//   .btn-dogecoin{
+//     --doge-brand-color: #ba9f33;
+//     --doge-shadow-color: #e0cd81;
+//     --doge-text-color: #ffffff;
+//     border-color:var(--doge-brand-color) !important}
+    
+//   .btn-dogecoin
+//     ::before{
+//       background:
+//       var(--doge-brand-color);
+//       border-radius:100%;
+//       color:var(--doge-text-color);
+//       content:'茐';
+//       display:inline-block;
+//       font-size:16px;
+//       font-weight:600;
+//       height:22px;
+//       line-height:22px;
+//       margin-right:5px;
+//       padding:1px 0 0 2px;
+//       width:21px
+//     }
+//     .btn-dogecoin 
+//     .dogecoin-wordmark{fill:#171a20}
+//     .btn-dogecoin
+//     :hover,
+//     .btn-dogecoin:
+//       focus{background:var(--doge-brand-color)
+//         ;border-color:var(--doge-brand-color);
+//         box-shadow:var(--doge-shadow-color) 0 0 8px}
+//         .btn-dogecoin:hover svg,.btn-dogecoin:focus svg{filter:drop-shadow(0 0 8px var(--doge-shadow-color))}.btn-dogecoin:hover .dogecoin-wordmark,
+//         .btn-dogecoin:focus .dogecoin-wordmark{fill:var(--doge-text-color)}.btn-dogecoin:hover::before,
+//         .btn-dogecoin:focus::before{background:var(--doge-text-color) !important;color:var(--doge-brand-color) 
